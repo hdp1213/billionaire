67a4fffdc918c0ef5cc0dd691ce281dfa50a7040
hdp1213 <hdp1213@hotmail.com>
2017-11-07 00:04:07 +1030

    Add simple ASK algorithm
    This particular algorithm asks for the maximum number of cards it has in
    the hand. Just a proof-of-concept to demonstrate some Cards methods.
    
    There is a slight issue in that the bot sends an ASK command with no
    cards in it before it begins going through the cards in its hand. I feel
    like it is an issue with the asyncio.Queue used with commands, and how
    the issue_command() is called once before everything begins, but I
    could not tell you as the queue seems to be empty when the bot JOINs
    and STARTs the game. Something to keep in mind, I guess.

--- python/bots.py
+++ python/bots.py
@@ -1,10 +1,16 @@
 from base import BaseBillionaireBot
 
-from card import Card
+from card import Card, CardType, Commodity
 from command import Command, CommandList
 
 
 class DumbBot(BaseBillionaireBot):
     async def issue_command(self):
-        ask = Command(Command.ASK, cards=[1])
+        try:
+            card_id, amt = self.hand.most_common(1)[0]
+        except IndexError:
+            card_id, amt = None, 0
+
+        cards = self.hand.take_card(card_id, amt)
+        ask = Command(Command.ASK, cards=cards.to_dict())
         return ask
